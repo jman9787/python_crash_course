215898b5af231d93f7c75e042de9c14c423c4bae
jman9787 <jman9545@gmail.com>
2025-09-22 00:30:15 -0500

    stopping point for the night, getting tired

---
+++ Chapter 5 If Statements.py
@@ -0,0 +1,12 @@
+requested_topping = 'mushroom'
+
+if requested_topping != 'anchovies':
+    print("hold the anchovies!!")
+
+
+
+banned_users = ['andrew', 'carolina', 'bryan']
+user = 'marie'
+
+if user not in banned_users:
+    print(f'{user.capitalize()} you may post a response if you wish')
